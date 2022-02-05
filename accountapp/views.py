7570b0f84cd3d825707808a5601d7b887823eab8
gabang2 <offbeat1020@naver.com>
2022-02-05 21:09:23 +0900

    26th class

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
-
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == "POST":
@@ -24,13 +25,28 @@ def hello_world(request):
         HelloWorld_list = HelloWorld.objects.all
         return render(request, 'accountapp/hello_world.html', context={'hello_world_input': HelloWorld_list})
 
+
 class AccountCreateView(CreateView):
     model = User
-    form_class = UserCreationForm #기본적인 userform을 제공해준다.
-    success_url = reverse_lazy('accountapp:login') #reverse는 함수형, reverse_lazy는 class에서 사욯한다.
+    form_class = UserCreationForm  # 기본적인 userform을 제공해준다.
+    success_url = reverse_lazy('accountapp:login')  # reverse는 함수형, reverse_lazy는 class에서 사욯한다.
     template_name = 'accountapp/create.html'
+
 
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
