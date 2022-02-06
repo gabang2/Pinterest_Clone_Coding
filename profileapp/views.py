1401e33788d7e27bf5097bd934b3a925b122fea8
gabang2 <offbeat1020@naver.com>
2022-02-06 21:51:40 +0900

    33rd class

--- profileapp/views.py
+++ profileapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -17,7 +17,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -26,6 +25,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
 
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})  # self.object는 user임.
 
 
 @method_decorator(profile_ownership_required, 'get')
@@ -36,3 +38,6 @@ class ProfileUpdateView(UpdateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})  # self.object는 user임.
