63b4723351f817679a705d7886a339fd67bdbab3
gabang2 <offbeat1020@naver.com>
2022-02-07 09:41:20 +0900

    35th class

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    # set_null은 게시글이 사라졌을 때 게시글 삭제x
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+
+    title = models.CharField(max_length=200, null=True)
+    content = models.TextField(null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
--- profileapp/views.py
+++ profileapp/views.py
@@ -27,7 +27,8 @@ class ProfileCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})  # self.object는 user임.
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+        # self.object는 user임.
 
 
 @method_decorator(profile_ownership_required, 'get')
