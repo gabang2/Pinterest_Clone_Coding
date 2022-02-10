13de7470aa7305690b2b8262d85347234ee7bfb6
gabang2 <offbeat1020@naver.com>
2022-02-10 21:27:35 +0900

    43rd class

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -13,20 +12,28 @@ from subscribeapp.models import Subscription
 @method_decorator(login_required, 'get')
 class SubscriptionView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
-        # subscribe.html페이지에서 project_pk라는 값으로 get인자를 보낼 때, kwargs에 pk라는 값으로 저장하여 projectapp:detail의 pk값으로 들어가게 됨.
         return reverse('projectapp:detail', kwargs={'pk': self.request.GET.get('project_pk')})
 
     def get(self, request, *args, **kwargs):
-        # project_pk를 가진 project를 찾을건데 만약 없다면 404예외처리를 함.
         project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
         user = self.request.user
+        subscription = Subscription.objects.filter(project=project, user=user)
 
-        # 이제 project와 user정보를 찾았으니 subscription을 해줄 것임.
-        subscription = Subscription.objects.filter(user=user, project=project)
-
-        # 있으면 없애기, 없으면 존재하게 만들기
         if subscription.exists():
             subscription.delete()
         else:
             Subscription(user=user, project=project).save()
         return super(SubscriptionView, self).get(request, *args, **kwargs)
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    model = Article
+    template_name = 'subscribeapp/list.html'
+    context_object_name = 'article_list'
+    paginate_by = 5
+
+    def get_queryset(self):
+        projects = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=projects)
+        return article_list
