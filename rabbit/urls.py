33b9394d7eafa2200eeb0e48f5910e51b851b821
gabang2 <offbeat1020@naver.com>
2022-03-09 23:58:59 +0900

    happy

--- rabbit/urls.py
+++ rabbit/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import pandas
 
 from articleapp.views import ArticleListView
 
